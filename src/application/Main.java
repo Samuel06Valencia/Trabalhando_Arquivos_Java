e3e0ca4e8dddb0c8dad8c3b1c11a7cfe9e263120
Samuel Silva Valencia <sv807227@gmail.com>
2024-01-04 12:14:58 -0300

    <chore> [Terceira Versão]: <Terceira versão, comentário modificado.>

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Segunda versão, leitura manual.
+        //Terceira versão, leitura automatizada.
 
         String path = "D:\\temp\\in.txt"; //String com caminho do arquivo.
 

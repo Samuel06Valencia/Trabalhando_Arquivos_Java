cbcd0b8ccf1479662317454971ece72a0247c502
Samuel Silva Valencia <sv807227@gmail.com>
2024-01-04 18:04:35 -0300

    <Feat> [Última versão do projeto]: <Nesta versão implemenmtei um projeto de leitura de arquivo .csv>

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,27 +1,43 @@
 package application;
 
+import entities.Product;
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Terceira versão, leitura automatizada.
+        //Projeto Leitura de arquivo .csv
+        String path = "D:\\temp\\in.txt";
 
-        String path = "D:\\temp\\in.txt"; //String com caminho do arquivo.
+        List<Product> list = new ArrayList<Product>(); //Lista para receber produtos.
 
-        try(BufferedReader br = new BufferedReader(new FileReader(path))) { //(try with resources,permite instanciar recursos na linha do try, fazendo com que esses recursos sejam desalocados de forma automatizada quando o escopo do try acabar.
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) { //Estrutura try-with-resources, fazendo com que os recursos sejam desalocados de forma automatizada. 
 
-            String line = br.readLine();
-            while(line != null) {
-                System.out.println(line);
-                line = br.readLine();
+            String line = br.readLine(); //Variável para ler a primeira linha
+            line = br.readLine();
+            while(line != null) { //Enquanto a linha for diferente de nulo...
+
+                String[] vect = line.split(","); //vai recortar o string em partes,onde ele achar a vírgula(",").
+                String name = vect[0]; //posição 0 na linha String possui o nome do arquivo.
+                Double price = Double.parseDouble(vect[1]); //posição 1 na linha String possui o price do produto.
+                Integer qte = Integer.parseInt(vect[2]); //posição 2 na linha String possui a quantity do produto.
+
+                Product prod = new Product(name, price, qte); //instanciação da classe produto.
+                list.add(prod); //adiciona a instanciação do objeto a lista "list" declarada a cima.
+
+                line = br.readLine(); //Leia a próxima linha...
+
             }
-
-            br.close();
+            System.out.println("PRODUCTS:");
+            for (Product p : list) { //percorre a list de produtos
+                System.out.println(p); //imprime as listas
+            }
         }
         catch(IOException e) {
-            System.out.println("Error: "+e.getMessage());
+            System.out.println("Error: "+e.getMessage()); //tratamento exceção
         }
-
     }
 }
